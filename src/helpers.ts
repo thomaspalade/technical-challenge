d2708d75e9870a1f204db1992ebb7028edc28150
Thomas Palade <thomas.e.palade@gmail.com>
2023-01-23 18:49:06 +0200

    feat(helpers): add toFailure, toSuccess and isSupportedLanguage functions

---
+++ src/helpers.ts
@@ -0,0 +1,21 @@
+import { SUPPORTED_LANGUAGES } from './enums';
+import { Failure, Success } from './generic-types';
+
+export const toFailure = (status: number, message: string): Failure<Error> => ({
+  ok: false,
+  status,
+  error: new Error(message),
+});
+
+export const toSuccess = <T>(status: number, value: T): Success<T> => ({
+  ok: true,
+  status,
+  value,
+});
+
+export const isSupportedLanguage = (language: SUPPORTED_LANGUAGES): boolean =>
+  [
+    SUPPORTED_LANGUAGES.CHINESE,
+    SUPPORTED_LANGUAGES.ENGLISH,
+    SUPPORTED_LANGUAGES.GERMAN,
+  ].includes(language);
