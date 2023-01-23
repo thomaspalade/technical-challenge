6582a55e897887feaa7cb5cb822e517312501eae
Thomas Palade <thomas.e.palade@gmail.com>
2023-01-23 18:22:21 +0200

    feat(types): add Failure, Success and Either types

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export type Failure<T = Error> = {
+  readonly ok: false;
+  readonly status: number;
+  readonly error: T;
+};
+
+export type Success<T> = {
+  readonly ok: true;
+  readonly status: number;
+  readonly value: T;
+};
+
+export type Either<E, A> = Failure<E> | Success<A>;
