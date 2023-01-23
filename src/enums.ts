b53a3fc3b4df9d7a11c9869a93c24cc62a89b1e0
Thomas Palade <thomas.e.palade@gmail.com>
2023-01-23 19:05:04 +0200

    feat(errors): add ERROR_MESSAGES enum

--- src/enums.ts
+++ src/enums.ts
@@ -3,3 +3,10 @@ export enum SUPPORTED_LANGUAGES {
   ENGLISH = 'en-gb',
   GERMAN = 'de',
 }
+
+export enum ERROR_MESSAGES {
+  CACHED_DATA_NOT_AVAILABLE = 'Cached data not available',
+  DATA_NOT_AVAILABLE = 'Data could not be retrieved from URL',
+  EVENT_NOT_FOUND = 'Event not found',
+  SPORTS_NOT_FOUND = 'Sports not found for the current language',
+}
