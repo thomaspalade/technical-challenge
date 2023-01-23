b964bf2232a0f84fd6170682b73f3c09900e31e2
Thomas Palade <thomas.e.palade@gmail.com>
2023-01-23 19:29:54 +0200

    feat(events): add business logic for the getEvents request handler

--- src/events/api/get-events.ts
+++ src/events/api/get-events.ts
@@ -1,10 +1,34 @@
 import { Request, RequestHandler, Response } from 'express';
+import { getCacheData } from '../../cache/cache';
+import { ENV_CONFIG } from '../../env';
+import { ERROR_MESSAGES } from '../../enums';
 
 export const getEvents: RequestHandler = async (
   req: Request,
   res: Response
 ) => {
+  const maybeCachedData = await getCacheData(
+    ENV_CONFIG.cacheInstance,
+    ENV_CONFIG.victorBetBaseURL,
+    ENV_CONFIG.victorBetSuffixURL
+  );
+  if (!maybeCachedData.ok) {
+    res.status(maybeCachedData.status).send({
+      message: ERROR_MESSAGES.CACHED_DATA_NOT_AVAILABLE,
+    });
+    return;
+  }
+
+  const cachedData = maybeCachedData.value;
+  if (req.query && req.query.sportId) {
+    res.status(200).send({
+      eventsBySportId: cachedData.get('sport_' + req.query.sportId + '_events'),
+    });
+    return;
+  }
+
   res.status(200).send({
-    allEvents: [],
+    allEvents: cachedData.get('allEvents'),
   });
+  return;
 };
