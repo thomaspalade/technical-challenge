db1297b7f9a426cdec9f4e43f5a0b6c9df770a92
Thomas Palade <thomas.e.palade@gmail.com>
2023-01-23 19:07:19 +0200

    feat(cache): add function that checks if cache contains data for a given language

--- src/cache/cache.ts
+++ src/cache/cache.ts
@@ -1,6 +1,16 @@
 import NodeCache from "node-cache";
+import { SUPPORTED_LANGUAGES } from "../enums";
 
 export const cleanUpCache = (cache: NodeCache): void => {
   cache.flushAll();
   cache.close();
 };
+
+export const cacheContainsDataForLanguage = 
+  (cache: NodeCache, lang: SUPPORTED_LANGUAGES): boolean => {
+    const result = cache.get(`${lang}_sortedSports`);
+    if (!result) {
+      return false;
+    }
+    return Object.keys(result).length !== 0;
+  };
