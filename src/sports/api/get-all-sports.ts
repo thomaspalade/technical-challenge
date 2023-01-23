e5bb050eed81e6f04e6aedc2d5d79cd60a750a44
Thomas Palade <thomas.e.palade@gmail.com>
2023-01-23 19:31:09 +0200

    feat(sports): add business logic for the getAllSports

--- src/sports/api/get-all-sports.ts
+++ src/sports/api/get-all-sports.ts
@@ -1,10 +1,27 @@
 import { Request, RequestHandler, Response } from 'express';
+import { ENV_CONFIG } from '../../env';
+import { getCacheData } from '../../cache/cache';
+import { DEFAULT_LANGUAGE } from '../../const';
+import { ERROR_MESSAGES } from '../../enums';
 
 export const getAllSports: RequestHandler = async (
-  req: Request,
+  _req: Request,
   res: Response
 ) => {
+  const maybeCachedData = await getCacheData(
+    ENV_CONFIG.cacheInstance,
+    ENV_CONFIG.victorBetBaseURL,
+    ENV_CONFIG.victorBetSuffixURL
+  );
+  if (!maybeCachedData.ok) {
+    res.status(maybeCachedData.status).send({
+      message: ERROR_MESSAGES.CACHED_DATA_NOT_AVAILABLE,
+    });
+    return;
+  }
+
+  const cachedData = maybeCachedData.value;
   res.status(200).send({
-    sortedSports: [],
+    sortedSports: cachedData.get(DEFAULT_LANGUAGE + '_sortedSports'),
   });
 };
