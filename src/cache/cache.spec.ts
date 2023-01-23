1b533f07be99d7c7a88f8c223a4dbc6e5b6540db
Thomas Palade <thomas.e.palade@gmail.com>
2023-01-23 20:43:18 +0200

    feat(tests): add some test for helpers and cache file

--- src/cache/cache.spec.ts
+++ src/cache/cache.spec.ts
@@ -1,11 +1,41 @@
 import test from 'ava';
+import NodeCache from 'node-cache';
+import { cacheContainsDataForLanguage, cleanUpCache } from './cache';
+import { SUPPORTED_LANGUAGES } from '../enums';
 
-test('When cleanUpCache is called, Then it returns 200', async (t) => {
-  t.true(true);
+test('When cleanUpCache is called, Then it removes all data from cache', async (t) => {
+  const cache = new NodeCache();
+  t.is(cache.getStats().keys, 0);
+
+  cache.set('key1', 10);
+  cache.set('key2', 20);
+  t.is(cache.getStats().keys, 2);
+
+  cleanUpCache(cache);
+  t.is(cache.getStats().keys, 0);
 });
 
-test('When cacheContainsDataForLanguage is called, Then it returns 200', async (t) => {
-  t.true(true);
+test('Given no data for any language inserted, When cacheContainsDataForLanguage is called, Then it returns false', async (t) => {
+  const cache = new NodeCache();
+  t.false(cacheContainsDataForLanguage(cache, SUPPORTED_LANGUAGES.CHINESE));
+  t.false(cacheContainsDataForLanguage(cache, SUPPORTED_LANGUAGES.ENGLISH));
+  t.false(cacheContainsDataForLanguage(cache, SUPPORTED_LANGUAGES.GERMAN));
+  cleanUpCache(cache);
+});
+
+test('Given data inserted for english language, When cacheContainsDataForLanguage is called with german or chinese as parameter, Then it returns false', async (t) => {
+  const cache = new NodeCache();
+  cache.set(SUPPORTED_LANGUAGES.ENGLISH + "_sortedSports", 100);
+  t.false(cacheContainsDataForLanguage(cache, SUPPORTED_LANGUAGES.GERMAN));
+  t.false(cacheContainsDataForLanguage(cache, SUPPORTED_LANGUAGES.CHINESE));
+  cleanUpCache(cache);
+});
+
+test('Given data inserted for english language, When cacheContainsDataForLanguage is called with english as parameter, Then it returns true', async (t) => {
+  const cache = new NodeCache();
+  cache.set(SUPPORTED_LANGUAGES.ENGLISH + "_sortedSports", 100);
+  t.true(cacheContainsDataForLanguage(cache, SUPPORTED_LANGUAGES.ENGLISH));
+  cleanUpCache(cache);
 });
 
 test('When getVictorBetResult is called, Then it returns 200', async (t) => {
--- src/helpers.spec.ts
+++ src/helpers.spec.ts
@@ -1,13 +1,37 @@
 import test from 'ava';
+import { isSupportedLanguage, toFailure, toSuccess } from './helpers';
+import { SUPPORTED_LANGUAGES } from './enums';
 
-test('When toFailure is called, Then it returns 200', async (t) => {
-  t.true(true);
+test('When `toFailure` is called with an object T, Then it returns an object of type Failure<T>', (t) => {
+  const res = toFailure(400, 'error message');
+  t.deepEqual(res, {
+    ok: false,
+    status: 400,
+    error: new Error('error message'),
+  });
 });
 
-test('When toSuccess is called, Then it returns 200', async (t) => {
-  t.true(true);
+test('When `toSuccess` is called with an object T, Then it returns an object of type Success<T>', (t) => {
+  const res = toSuccess(200, { message: 'expected data' });
+  t.deepEqual(res, {
+    ok: true,
+    status: 200,
+    value: { message: 'expected data' },
+  });
 });
 
-test('When isSupportedLanguage is called, Then it returns 200', async (t) => {
-  t.true(true);
+test('When isSupportedLanguage is called with english language as parameter, Then it returns true', async (t) => {
+  t.true(isSupportedLanguage(SUPPORTED_LANGUAGES.ENGLISH));
+});
+
+test('When isSupportedLanguage is called  with german language as parameter, Then it returns true', async (t) => {
+  t.true(isSupportedLanguage(SUPPORTED_LANGUAGES.GERMAN));
+});
+
+test('When isSupportedLanguage is called with chinese language as parameter, Then it returns true', async (t) => {
+  t.true(isSupportedLanguage(SUPPORTED_LANGUAGES.CHINESE));
+});
+
+test('When isSupportedLanguage is called with an unsupported language, Then it returns false', async (t) => {
+  t.false(isSupportedLanguage('fr' as SUPPORTED_LANGUAGES));
 });
